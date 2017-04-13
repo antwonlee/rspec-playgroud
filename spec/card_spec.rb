b81e057ab710e129edba524d1fc4b9372c347c82
antwonlee <antwonlee@gmail.com>
2017-04-13 08:59:56 -0500

    Organize files

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,22 +1,4 @@
-class Card
-  def initialize(suit:, rank:)
-    @suit = suit
-    @rank = case rank
-            when :jack then 11
-            when :queen then 12
-            when :king then 13
-            else rank
-            end
-  end
-
-  def suit
-    @suit
-  end
-
-  def rank
-    @rank
-  end
-end
+require 'card'
 
 RSpec.describe 'a playing card' do
   it 'has a suit' do
